00cfcbb5d5cde34d29bbd29554f0aac31b2d59c5
JMabbott <joelgwm@gmail.com>
2015-04-01 13:15:49 -0700

    Completed Tutorial 8
    Signed-off-by: JMabbott <joelgwm@gmail.com>

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -31,14 +31,65 @@ class Application extends CI_Controller {
      * Render this page
      */
     function render() {
-        $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
+        $this->data['menubar'] = $this->parser->parse('_menubar', $this->makemenu(),true);
         $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
-
+		$this->data['sessionid'] = session_id();
         // finally, build the browser page!
         $this->data['data'] = &$this->data;
         $this->parser->parse('_template', $this->data);
     }
+	
+	function restrict($roleNeeded = null) {
+		$userRole = $this->session->userdata('userRole');
+		if ($roleNeeded != null) {
+			if (is_array($roleNeeded)) {
+				if (!in_array($userRole, $roleNeeded)) {
+					redirect("/");
+					return;
+				}
+			} else if ($userRole != $roleNeeded) {
+				redirect("/");
+				return;
+			}
+		}
+	}
+	
+	function makemenu() {
+		//get role & name from session
+		$userRole = $this->session->userdata('userRole');
+		$userName = $this->session->userdata('name');
+		
+		// make array, with menu choice for alpha
+		$choices['menu_choices'] = array(
+			'menudata' => array(
+				array('name' => "Alpha", 'link' => '/alpha')
+				//array('name' => "Beta", 'link' => '/beta'),
+				//array('name' => "Gamma", 'link' => '/gamma'),
+				//array('name' => "Login", 'link' => '/auth'),
+				//array('name' => "Logout", 'link' => '/auth/logout')
+			)
+		);
 
+		// if user, add menu choice for beta and logout
+		if ($userRole == 'user') {
+			array_push($choices['menu_choices']['menudata'], array('name' => "Beta", 'link' => '/beta'));
+		}
+		
+		// if admin, add menu choices for beta, gamma and logout
+		if ($userRole == 'admin') {
+			array_push($choices['menu_choices']['menudata'], array('name' => "Beta", 'link' => '/beta'));
+			array_push($choices['menu_choices']['menudata'], array('name' => "Gamma", 'link' => '/gamma'));
+		}
+		
+		// add login or logout
+		if ($userRole == NULL)
+			array_push($choices['menu_choices']['menudata'], array('name' => "Login", 'link' => '/auth'));
+		else
+			array_push($choices['menu_choices']['menudata'], array('name' => "Logout", 'link' => '/auth/logout'));
+		
+		// return the choices array
+		return $choices['menu_choices'];
+	}
 }
 
 /* End of file MY_Controller.php */
